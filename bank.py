d83d4b02d4da2c14f5bb95415e53e243f3c755bb
Md. ASAD-AL-ADIL <119332365+asadaladil@users.noreply.github.com>
2023-06-15 17:00:30 +0600

    Add files via upload

--- bank.py
+++ bank.py
@@ -1,4 +1,4 @@
-class Admin:
+class Bank:
     Account=[]
     total_loan=0
     total_balance=50000000
@@ -12,12 +12,12 @@ class Admin:
         self.loan=0
         self.Account.append(name)
         self.transaction_history=[]
-        Admin.total_balance+=money
+        Bank.total_balance+=money
 
     def Deposit(self,amount):
         if amount>0:
             self.balance+=amount
-            Admin.total_balance+=amount
+            Bank.total_balance+=amount
             print(f'total balance is {self.balance}')
             a=f'you have dposit {amount}'
             self.transaction_history.append(a)
@@ -27,18 +27,19 @@ class Admin:
     def Withdraw(self,amount):
         if amount<=self.balance:
             self.balance-=amount
-            Admin.total_balance-=amount
+            Bank.total_balance-=amount
             print(f'take the money {amount} and your available balance is {self.balance-amount}')
             a=f'withdraw amount {amount}'
             self.transaction_history.append(a)
         else:
             print('bank is bankrupt')
+
     def Loan(self,amount):
-        if Admin.loan_system==True:
-            if(amount<=2*self.balance) and Admin.total_balance>amount:
+        if Bank.loan_system==True:
+            if(amount<=2*self.balance) and   Bank.total_balance>amount:
                 self.loan+=amount
-                Admin.total_loan+=amount
-                Admin.total_balance-=amount
+                Bank.total_loan+=amount
+                Bank.total_balance-=amount
                 print(f'take your money {amount} and your loan is {self.loan}')
                 a=f'you have take a loan of amount {amount}'
                 self.transaction_history.append(a)
@@ -60,20 +61,14 @@ class Admin:
         else:
             print('no account')
 
-    @staticmethod
-    def Total_loan():
-        print(f'Total loan is {Admin.total_loan}')
+    def Total_loan(self):
+        return Bank.total_loan
     
-    @staticmethod
-    def Total_balance():
-        print(f'total balance is {Admin.total_balance}')
-   
-    @staticmethod
-    def loan_system():
-        if Admin.loan_system==True:
-            Admin.loan_system=False
-        else:
-            Admin.loan_system=True
+    def Total_balance(self):
+        return Bank.total_balance
+    
+    def Loan_system(self,a):
+        Bank.loan_system=a
 
     def show_loan(self,name,password):
         if name in self.Account and self.password==password:
@@ -88,23 +83,19 @@ class Admin:
     def Transaction_history(self):
         for i in self.transaction_history:
             print(i)
-    @staticmethod
-    def Total_account():
-        print(len(Admin.Account))
-        for i in Admin.Account:
+    def Total_account(self):
+        print(len(Bank.Account))
+        for i in Bank.Account:
             print(i)
-        
-    
 
 
-class User(Admin):
+class User(Bank):
     def __init__(self, name, email, password, money) -> None:
-        super().__init__(name, email, password, money)
-    
+        super().__init__(name, email, password, money)  
     def Total_balance(self):
         return (f'You do not have access of it only Admin')
     def Total_loan(self):
-        return (f'You do not have access of it only admin')
+        return (f'You do not have access of it only Admin')
     def Deposit(self, amount):
         return super().Deposit(amount)
     def Withdraw(self, amount):
@@ -119,12 +110,20 @@ class User(Admin):
         return super().show_loan(name, password)
     def Transaction_history(self):
         return super().Transaction_history()
-    def loan_system(self):
+    def Loan_system(self,a):
         print(f'you do not have the access of it only Admin can see it')
     def Total_account(self):
         print('you can not show that only Admin can see it')
-    
-        
+
+class Admin(Bank):
+    def __init__(self, name, email, password, money) -> None:
+        super().__init__(name, email, password, money)
+    def Total_balance(self):
+        return super().Total_balance()
+    def Total_loan(self):
+        return super().Total_loan()
+    def Loan_system(self,a):
+        return super().Loan_system(a)
 
 
 
